c33031c8ce9a307063a51c4a52ec673ccc42d138
Cahya DSN <cahyadsn@gmail.com>
2021-03-04 13:57:52 +0700

    Update geo_ajax.php license statement
    mengubah pernyataan license ke MIT license

--- apps/inc/geo_ajax.php
+++ apps/inc/geo_ajax.php
@@ -4,19 +4,24 @@ BISMILLAAHIRRAHMAANIRRAHIIM - In the Name of Allah, Most Gracious, Most Merciful
 ================================================================================
 filename : geo_ajax.php
 purpose  :
-create   : 2017/09/12
-last edit: 190830,170927
+create   : 170912
+last edit: 210304
 author   : cahya dsn
 ================================================================================
 This program is free software; you can redistribute it and/or modify it under the
-terms of the GNU General Public License as published by the Free Software
-Foundation; either version 2 of the License, or (at your option) any later version.
+terms of the MIT License.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
-copyright (c) 2015-2019 by cahya dsn; cahyadsn@gmail.com
+See the MIT License for more details
+
+copyright (c) 2017-2021 by cahya dsn; cahyadsn@gmail.com
 ================================================================================*/
 include "db.php";
 $r=array('status'=>false,'error'=>'an error occured');
