afb4cda1190d88b19a5bf635cf2ae96ff849d257
Cahya DSN <cahyadsn@gmail.com>
2021-03-04 14:09:33 +0700

    Update geo_update.php license statement
    mengubah pernyataan license ke MIT dan menghapus field kodepos

--- apps/inc/geo_update.php
+++ apps/inc/geo_update.php
@@ -4,23 +4,28 @@ BISMILLAAHIRRAHMAANIRRAHIIM - In the Name of Allah, Most Gracious, Most Merciful
 ================================================================================
 filename : geo_update.php
 purpose  :
-create   : 2017/09/12
-last edit: 190830,170927
+create   : 170912
+last edit: 210304
 author   : cahya dsn
 ================================================================================
 This program is free software; you can redistribute it and/or modify it under the
-terms of the GNU General Public License as published by the Free Software
-Foundation; either version 2 of the License, or (at your option) any later version.
+terms of the MIT License.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
-copyright (c) 2017-2019 by cahya dsn; cahyadsn@gmail.com
+See the MIT License for more details
+
+copyright (c) 2017-2021 by cahya dsn; cahyadsn@gmail.com
 ================================================================================*/
 include "db.php";
 $r=array('status'=>false,'msg'=>'do nothing');
-$fields=array('lat','lng','kodepos','path');
+$fields=array('lat','lng','path');
 if(isset($_POST['id'])){
   $sql="UPDATE {$tbl_wilayah} SET ";
   $field=$data=array();
